aa56c6f18ff83faf84941b0fbd7d9a01f12ecdcf
Sarah Dykstra <sarahdykstra1876@gmail.com>
2020-09-13 01:18:45 -0400

    Updates

---
+++ precache-manifest.7358cd78c8b277454b06d749000cd97a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aff8cf4b636286994d02cf9ac4a8bbb",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "69817dd3d376eb0f5ad5",
+    "url": "/my-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8fa05a725ce0020bc0c0",
+    "url": "/my-portfolio/static/js/2.4a91bcbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-portfolio/static/js/2.4a91bcbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69817dd3d376eb0f5ad5",
+    "url": "/my-portfolio/static/js/main.d111ca39.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  }
+]);
